d167f4110f1d23b9df1994b9dd13a354c980f5d4
Nurbek Chymbaev <n.chymbaev@unitpay.ru>
2019-09-17 13:22:33 +0300

    added payment signature

--- unitpay/wa-plugins/payment/unitpay/lib/config/plugin.php
+++ unitpay/wa-plugins/payment/unitpay/lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'icon'        => 'img/unitpay16.png',
     'logo'        => 'img/unitpay.png',
     'vendor'      => 'webasyst',
-    'version'     => '1.0.0',
+    'version'     => '1.0.1',
     'locale'      => array('ru_RU',),
     'type'        => waPayment::TYPE_ONLINE,
 );
